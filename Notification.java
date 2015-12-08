13cf8735baa7c30b5aef2e93d708d465e49d141b
RicoBoone <Ricbun@MacBook-Pro-van-Rico.local>
2015-12-08 15:58:36 +0100

    Merged notification classes

---
+++ Notification.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+import TI.*;
+public class Notification
+{
+    private LED led;
+    
+    public Notification()
+    {
+        led = new LED();
+    }
+    public void forward()
+    {
+        led.ledLF(Color.blue);
+        led.ledMF(Color.blue);
+        led.ledRF(Color.blue);
+    }
+    public void backwards()
+    {
+        led.ledLF(Color.blue);
+        led.ledMF(Color.blue);
+        led.ledRF(Color.blue);
+    }
+    public void TurnLeft()
+    {
+        led.ledLF(Color.blue);
+        led.ledLB(Color.blue);
+    }
+    public void TurnRight()
+    {
+        led.ledRB(Color.blue);
+        led.ledRF(Color.blue);
+    }
+    public void bounce()
+    {
+        led.ledLF(Color.red);
+        led.ledMF(Color.red);
+        led.ledRF(Color.red);
+    }
+    public void detect()
+    {
+        led.ledLF(Color.red);
+        led.ledMF(Color.red);
+        led.ledRF(Color.red);
+    }
+    public void reset()
+    {
+        led.ledLF(Color.black);
+        led.ledMF(Color.black);
+        led.ledRF(Color.black);
+    }
+}
