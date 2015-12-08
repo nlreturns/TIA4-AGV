ebbb85e507dd9c5134430c65fd5901f837aac08c
RicoBoone <Ricbun@MacBook-Pro-van-Rico.local>
2015-12-08 14:15:03 +0100

    Line detection added
    lol

---
+++ Linedetection.java
@@ -0,0 +1,37 @@
+import TI.*;
+
+public class Linedetection
+{
+
+    static Motor motor;
+    
+    public Linedetection()
+    {
+        motor = new Motor(14, 15);
+    }
+    
+    public static void lineRider()
+    {
+        if(BoeBot.analogRead(0) > 700){
+            motor.setLeft(1550);
+            motor.setRight(1550);
+        } else {    
+            if(BoeBot.analogRead(2) > 700){
+                motor.setLeft(1450);
+                motor.setRight(1450);
+            } else {
+                motor.setLeft(1450);
+                motor.setRight(1550);
+            }
+        }
+    }
+    
+    public static boolean detectCross()
+    {
+        if((BoeBot.analogRead(0) > 700) && (BoeBot.analogRead(0) > 700) && (BoeBot.analogRead(0) > 700)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
