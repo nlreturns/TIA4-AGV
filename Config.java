c4b6659581b44892fc82c3c8b6e3f9a2833fdddb
nlreturns <janwillem.dooms@gmail.com>
2015-12-14 14:03:49 +0100

    Config first push
    Not implemented yet

---
+++ Config.java
@@ -0,0 +1,17 @@
+public class Config
+{
+    /**
+     * Pins configuration
+     */
+    public static final int ULTRASOONINPUT = 6;
+    public static final int ULTRASOONOUTPUT = 7;
+    public static final int RIGHTSENSOR = 8;
+    public static final int LEFTSENSOR = 9;
+    public static final int INFRARED = 13;
+    public static final int LEFTMOTOR = 14;
+    public static final int RIGHTMOTOR = 15;
+    // state config
+    public static final int STARTINGSTATE = 2;
+    
+    
+}
