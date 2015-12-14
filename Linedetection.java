920597c6417773741183745b046a7f4d698ed887
perryledoux <perry1995@live.com>
2015-12-14 13:52:41 +0100

    kakje

--- Linedetection.java
+++ Linedetection.java
@@ -1,8 +1,11 @@
+
 import TI.*;
 
 public class Linedetection
 {
-
+    
+    private int speedLeft;
+    private int speedRight;
     static Drive drive;
 
     public Linedetection()
@@ -11,21 +14,34 @@ public class Linedetection
     }
 
     public static void lineRider()
-    {
+    {       
+        
+        
         if(BoeBot.analogRead(0) > 700){
-            drive.rotateLeft(1550, 1550);
-        } else {    
-            if(BoeBot.analogRead(2) > 700){
-                drive.rotateRight(1450, 1450);
-            } else {
-                drive.forward(1450, 1550);
+           drive.rotateLeft(1495,1700);
+          
+            //naar rechts
+        }
+        else{
+            if (BoeBot.analogRead(2) > 700){
+                drive.rotateRight(1300,1505);
+            }
+            else{
+                if (BoeBot.analogRead(1)  > 700){
+                 drive.rotateLeft(1400, 1600);
+                    //halve snelheid vooruit
+                }
+                else  {
+                  drive.rotateLeft(1400, 1600);
+                }
             }
         }
+        
     }
-
+    
     public static boolean detectCross()
     {
-        if((BoeBot.analogRead(0) > 700) && (BoeBot.analogRead(0) > 700) && (BoeBot.analogRead(0) > 700)) {
+        if((BoeBot.analogRead(0) > 700) && (BoeBot.analogRead(1) > 700) && (BoeBot.analogRead(2) > 700)) {
             return true;
         } else {
             return false;
