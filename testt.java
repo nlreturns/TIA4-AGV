a1ccd91350cade17ee8c97652070444d1830eaf0
nlreturns <janwillem.dooms@gmail.com>
2016-01-04 11:48:53 +0100

    hardcoded class

---
+++ testt.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class testt here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class testt
+{
+    static Linedetection line = new Linedetection();
+    /**
+     * Constructor for objects of class testt
+     */
+    public static void main (String[] args){
+        line.lineRider();
+    }
+
+}
